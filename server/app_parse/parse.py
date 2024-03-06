82a4a4a89dbf08d064f878b19abb96a61bdc9d07
biggvladik <zuevvlad55@gmail.com>
2024-03-06 23:35:03 +0300

    Организовал простейшее кэширование с использованием redis

--- server/app_parse/parse.py
+++ server/app_parse/parse.py
@@ -1,8 +1,14 @@
 import aiohttp
-
+import redis
 async def get_current_price(money_id:int)->dict:
     async with aiohttp.ClientSession() as session:
         async with session.get(f'https://api.coinlore.net/api/ticker/?id={money_id}') as response:
             res = await response.json()
             return res
+
+
+def create_redis_conn():
+    pub= redis.Redis(host='localhost', port=6379)
+    return pub
+
 
--- server/app_parse/routers.py
+++ server/app_parse/routers.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
-from .parse import get_current_price
-import asyncio
-
+from .parse import get_current_price,create_redis_conn
+import json
 router = APIRouter()
+redis_cache = create_redis_conn()
+
 @router.get("/get_price{id}", response_description= "get price crypto by id")
 async def get_price(money_id:int):
-   res = await get_current_price(money_id)
-   return res
+   data = redis_cache.get(str(money_id))
+   if data:
+      return json.loads(data)
+   else:
+      res = await get_current_price(money_id)
+      text_res = json.dumps(res)
+      redis_cache.set(f'{money_id}',text_res,ex=2)
+      return res
 
